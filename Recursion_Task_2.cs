05d1bd37b0e0056a89f49cb5639b6469aa19ff8d
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-15 09:19:41 +0100

    Update Recursion_Task_2.cs

--- Recursion_Task_2.cs
+++ Recursion_Task_2.cs
@@ -4,6 +4,10 @@ namespace Lab_2_2
 {
 	class Program
 	{
+		/*
+		   Task:
+		   Implement a recursive algorithm for calculating 10 Fibonacci numbers
+		*/
 		public static int Fibonachi(int number)
 		{
 			if (number == 0 || number == 1) return 1;
